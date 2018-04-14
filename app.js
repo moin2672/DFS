f14e0e956150b3a010ea914f8ed1bae5d862aff0
moin2672 <moin2672@gmail.com>
2018-04-14 19:26:10 +0530

    fourth commit

--- app.js
+++ app.js
@@ -588,7 +588,7 @@ app.get('/file1/:id', function(req, res){
 
 
 
-const port = 5005;
+const port = process.env.PORT | 5005;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
